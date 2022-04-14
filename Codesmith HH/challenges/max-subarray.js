38ff35dfdf896b05fdf0c6a13bc55c72a0ae9a98
Vincent <vincent.trang1@gmail.com>
2022-04-14 15:32:37 -0700

    completed max-subarray algo

--- Codesmith HH/challenges/max-subarray.js
+++ Codesmith HH/challenges/max-subarray.js
@@ -20,9 +20,50 @@ maxSubarray(input2);
 // returns 40 from subarray [15, 20, -5, 10]
 */
 
-const maxSubarray = (arr) => {
+//input arr
+//output a slice of the arr
+  //where the slice represents the largest sum of consecutive elements w/in the array
 
+//helper function that adds up the entire arr or subarr
+  //reduce method
+
+//sliding windows
+  //left one 
+  //right one
+
+//compare them against one another within a while loop
+  //while loop can increment the left or right window depending on what's greater 
+  //have a variable w/in scope to determine the greatest current sum 
+
+const maxSubarray = (arr) => {
+  
+  const helper = (arr) => {
+    return arr.reduce((acc, ele) => {
+      acc += ele;
+      return acc
+    }, 0) || -Infinity
+  }
+
+  let l = 0;
+  let r = arr.length;
+  let max = -Infinity;
+
+  while (l != r) {
+    let entire = helper(arr);
+    let left = helper(arr.slice(l, r - 1));
+    let right = helper(arr.slice(l + 1, r));
+
+    let greater = Math.max(entire, left, right);
+    if (greater > max) max = greater;
+    
+    if (left > right) r -= 1;
+    else l += 1;
+  }
+
+  return max
 }
+
+console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]))
 
 /*
   Extension: solve in linear time and constant space
