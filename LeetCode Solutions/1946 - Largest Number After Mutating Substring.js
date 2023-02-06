c4f85c632354102dd66690485d7a30eea88016f5
Vincent <vincent.trang1@gmail.com>
2023-02-06 12:46:51 -0800

    completed largest number after mutating substring

---
+++ LeetCode Solutions/1946 - Largest Number After Mutating Substring.js
@@ -0,0 +1,73 @@
+// You are given a string num, which represents a large integer. You are also given a 0-indexed integer array change of length 10 that maps each digit 0-9 to another digit. More formally, digit d maps to digit change[d].
+
+// You may choose to mutate a single substring of num. To mutate a substring, replace each digit num[i] with the digit it maps to in change (i.e. replace num[i] with change[num[i]]).
+
+// Return a string representing the largest possible integer after mutating (or choosing not to) a single substring of num.
+
+// A substring is a contiguous sequence of characters within the string.
+
+ 
+
+// Example 1:
+
+// Input: num = "132", change = [9,8,5,0,3,6,4,2,6,8]
+// Output: "832"
+// Explanation: Replace the substring "1":
+// - 1 maps to change[1] = 8.
+// Thus, "132" becomes "832".
+// "832" is the largest number that can be created, so return it.
+// Example 2:
+
+// Input: num = "021", change = [9,4,3,5,7,2,1,9,0,6]
+// Output: "934"
+// Explanation: Replace the substring "021":
+// - 0 maps to change[0] = 9.
+// - 2 maps to change[2] = 3.
+// - 1 maps to change[1] = 4.
+// Thus, "021" becomes "934".
+// "934" is the largest number that can be created, so return it.
+// Example 3:
+
+// Input: num = "5", change = [1,4,7,5,3,2,5,6,9,4]
+// Output: "5"
+// Explanation: "5" is already the largest number that can be created, so return it.
+
+//input string, array
+//output string
+    //that represents the input string that has been mutated (or not) to its largest possible value 
+        //can mutate based on the string integer's index w/ respect to input array
+
+//strat
+    //split num into array so that we can mutate it
+    //create a helper func that takes in 1 arg (i)
+        //if num[i] less change[num[i]] update and then recurse
+        //else return num.join
+    //iteriate through num
+        //if num[i] is less than change
+            //return helper
+    //return num
+
+var maximumNumber = function(num, change) {
+  //split, so that we can mutate
+  num = num.split('');
+
+  //helper function that will update the input string and recursively call itself
+  const helper = i => {
+      const current = Number(num[i]);
+      //will recursively call if the number of change is greater than the current element on num
+      if (current <= change[current]) {
+          num[i] = change[current];
+          return helper(i + 1);
+      //else we will return num, joined into a string
+      } else return num.join('')
+  }
+
+  for (let i = 0; i < num.length; i++) {
+      const current = Number(num[i]);
+      //will call helper where the number on change is greater than current element
+      if (current < change[current]) {
+          return helper(i)
+      }
+  }
+  return num.join('')
+};
